87b2851484242cb1b0db21f63cae6f682162f962
Chris72 <93304927+javito72@users.noreply.github.com>
2022-07-16 20:49:05 -0300

    inicio backend
    inicial backend

---
+++ Proyectov2/BackEnd/src/main/java/com/portfoliodos/javier/JavierApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliodos.javier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavierApplication.class, args);
+	}
+
+}
