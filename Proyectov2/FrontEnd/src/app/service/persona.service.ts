2a6a276fbf33f1de37400fa34109ebd2cea81c96
Chris72 <93304927+javito72@users.noreply.github.com>
2022-07-25 21:07:27 -0300

    update persona service
    url de heroku por el 8080

--- Proyectov2/FrontEnd/src/app/service/persona.service.ts
+++ Proyectov2/FrontEnd/src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.model';
 })
 export class PersonaService {
 
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://javito72.herokuapp.com//personas/';
 
   constructor(private http: HttpClient) { }
 
